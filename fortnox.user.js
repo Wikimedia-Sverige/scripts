1fa182503bee6be1efb18e2be4176270be17eed9
lokal-profil <lokal.profil@gmail.com>
2020-01-07 14:40:08 +0100

    bump version

--- fortnox.user.js
+++ fortnox.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Fortnox
-// @version  1.14
+// @version  1.15
 // @grant    none
 // @require  https://code.jquery.com/jquery-3.3.1.min.js
 // @include  https://apps*.fortnox.se/time/time_time/*
