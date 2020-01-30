50b8ba716bbeab319e41748fcb5deaa013b87f99
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2020-01-30 13:52:59 +0100

    Add Greasemonkey script that adds a link to unread notifications in Phabricator
    The link appears as a bullhorn next to the bell icon, if there are any
    unread notifications.

---
+++ phabricator.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name     Phabricator
+// @version  1.0
+// @grant    none
+// @require  https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @include  https://phabricator.wikimedia.org/*
+// ==/UserScript==
+
+$(function() {
+    $("<a></a>")
+        .attr("href", "/notification/query/unread/")
+        .addClass("alert-notifications")
+        .append(
+            $("<span></span>")
+                .addClass("phabricator-main-menu-message-icon phui-icon-view phui-font-fa fa-bullhorn")
+        )
+        .addClass("alert-notifications")
+        .insertAfter(".alert-unread");
+});
