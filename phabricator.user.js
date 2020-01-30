f58a8f760ee28f975a3269f8c06628be972aff07
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2020-01-30 17:04:53 +0100

    Code review follow up

--- phabricator.user.js
+++ phabricator.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name     Phabricator
-// @version  1.0
-// @grant    none
-// @require  https://code.jquery.com/jquery-3.4.1.slim.min.js
-// @include  https://phabricator.wikimedia.org/*
+// @name        Phabricator
+// @version     1.0
+// @grant       none
+// @require     https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @include     https://phabricator.wikimedia.org/*
+// @downloadURL https://github.com/Wikimedia-Sverige/scripts/raw/master/phabricator.user.js
 // ==/UserScript==
 
 $(function() {
-    $("<a></a>")
-        .attr("href", "/notification/query/unread/")
-        .addClass("alert-notifications")
-        .append(
-            $("<span></span>")
-                .addClass("phabricator-main-menu-message-icon phui-icon-view phui-font-fa fa-bullhorn")
-        )
-        .addClass("alert-notifications")
-        .insertAfter(".alert-unread");
+  $("<a></a>")
+  	.attr("href", "/notification/query/unread/")
+  	.addClass("alert-notifications alert-unread")
+  	.append(
+    	$("<span></span>")
+    		.addClass("phabricator-main-menu-alert-icon phui-icon-view phui-font-fa fa-bullhorn")
+  	)
+  	.addClass("alert-notifications")
+  	.insertAfter(".alert-unread");
 });
