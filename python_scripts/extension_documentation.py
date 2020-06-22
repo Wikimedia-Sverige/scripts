d3d4e3b3bcf67f4de56d4c8922a1448b2f23c392
Sebastian Berlin <sebastian.berlin@wikimedia.se>
2020-06-22 10:49:05 +0200

    Read config descriptions from extension.json
    Supports manifest version 1 and 2.

--- python_scripts/extension_documentation.py
+++ python_scripts/extension_documentation.py
@@ -22,6 +22,7 @@ import sys
 from collections import OrderedDict
 import subprocess
 import re
+import argparse
 
 
 RE_HOOK_FUNCTION = re.compile(r"^\tpublic static function on([^\(]+)\(")
@@ -45,28 +46,41 @@ def print_extension_json_info(extension_json_file):
     config = extension_dict["config"]
     headers_string = """{| class="wikitable"
 |-
-!Option
-!Default value
-!Documentation"""
+! Option
+! Default value
+! Documentation"""
     print(headers_string)
     parameters = []
     for key, value in config.items():
+        documentation = "DOCUMENTATION GOES HERE"
+        if args.manifest_version == 1:
+            default = json.dumps(value, indent=4)
+        elif args.manifest_version == 2:
+            if "description" in value:
+                documentation = value["description"]
+                if type(documentation) == list:
+                    documentation = "".join(documentation)
+            default = json.dumps(value["value"], indent=4)
         row = """|-
 | {option}
 | <source lang="json">{default}</source>
-| DOCUMENTATION GOES HERE"""\
-        .format(option=key, default=json.dumps(value, indent=4))
+| {documentation}"""\
+        .format(
+            option=key,
+            default=default,
+            documentation=documentation
+        )
         print(row)
         parameters.append("* $wg{}".format(key))
     print("|}")
     print()
 
     print("===Infobox===")
-    print("|version = {}".format(extension_dict["version"]))
+    print("| version = {}".format(extension_dict["version"]))
     print()
-    print("|update = {}".format(get_update_date()))
+    print("| update = {}".format(get_update_date()))
     print()
-    print("|parameters = \n{}".format("\n".join(parameters)))
+    print("| parameters = \n{}".format("\n".join(parameters)))
     print()
     print_hooks()
 
@@ -76,7 +90,7 @@ def get_update_date():
 
     """
     log = subprocess.run(
-        ["git", "-C", extension_path, "log", "--date=short"],
+        ["git", "-C", args.extension_path, "log", "--date=short"],
         stdout=subprocess.PIPE,
         universal_newlines=True
     ).stdout
@@ -89,23 +103,19 @@ def print_hooks():
 
     """
     i = 1
-    with open("{}/WikispeechHooks.php".format(extension_path)) as f:
+    with open("{}/WikispeechHooks.php".format(args.extension_path)) as f:
         for line in f:
             match = RE_HOOK_FUNCTION.match(line)
             if match:
-                print("|hook{} = {}".format(i, match.group(1)))
+                print("| hook{} = {}".format(i, match.group(1)))
                 i += 1
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Extension directory must be supplied.")
-        sys.exit(1)
-    extension_path = sys.argv[1]
-    extension_json_path = "{}/extension.json".format(extension_path)
-    try:
-        extension_json_file = open(extension_json_path)
-    except:
-        print("Failed to open file: {}".format(extension_json_path))
-        sys.exit(1)
-    print_extension_json_info(extension_json_file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--extension-path", "-e", default=".")
+    parser.add_argument("--manifest-version", "-m", default="2", type=int)
+    args = parser.parse_args()
+    extension_json_path = "{}/extension.json".format(args.extension_path)
+    with open(extension_json_path) as extension_json_file:
+        print_extension_json_info(extension_json_file)
